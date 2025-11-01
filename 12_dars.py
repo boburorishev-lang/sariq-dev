9cfcd961e700dde9c148a59e55715496d908a5e2
boburorishev-lang <boburorishev@gmail.com>
2025-11-01 22:11:08 +0500

    Add files via upload

--- 12_dars.py
+++ 12_dars.py
@@ -1,23 +1,23 @@
 #1_topshiriq
 
-# odam={"ism":"Bobur",
-#       "yil":"2001",
-#       "tugilgan_joy":"Toshkent"
-#       }
-# print(f"Mani ismim {odam['ism']}, {odam['yil']}-yilda, {odam['tugilgan_joy']} shaxrida tug'ilganman")
+odam={"ism":"Bobur",
+      "yil":"2001",
+      "tugilgan_joy":"Toshkent"
+      }
+print(f"Mani ismim {odam['ism']}, {odam['yil']}-yilda, {odam['tugilgan_joy']} shaxrida tug'ilganman")
 
 
 #2_topshiriq
 
-# bollar={"Xasan":"osh",
-#         "Husan":"beshbarmoq",
-#         "Ali":"shorva",
-#         "Vali":"lag'mon",
-#         "G'ani":"manti"
-#         }
-# print(f"Xasanning sevimli taomi {bollar['Xasan']}")
-# print(f"Alining sevimli taomi {bollar['Ali']}")
-# print(f"G'anining sevimli taomi {bollar["G'ani"]}")
+bollar={"Xasan":"osh",
+        "Husan":"beshbarmoq",
+        "Ali":"shorva",
+        "Vali":"lag'mon",
+        "G'ani":"manti"
+        }
+print(f"Xasanning sevimli taomi {bollar['Xasan']}")
+print(f"Alining sevimli taomi {bollar['Ali']}")
+print(f"G'anining sevimli taomi {bollar["G'ani"]}")
 
 
 #3_topshiriq
@@ -37,8 +37,8 @@ python={"int":"butun son",
 
 #4_topshiriq
 
-# soz=input("Kalit so'zni kiriting: ").lower()
-# print(python.get(soz,"Bunday so'z mavjut emas"))
+soz=input("Kalit so'zni kiriting: ").lower()
+print(python.get(soz,"Bunday so'z mavjut emas"))
 
 
 #5_topshiriq
